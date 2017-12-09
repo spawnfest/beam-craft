bfb761777a61b823a0ec68929e45637a227c7bcc
angersock <angersock@angersock.com>
2017-12-08 19:36:09 -0600

    Finish basic login handler.

--- lib/beam_craft/game_server.ex
+++ lib/beam_craft/game_server.ex
@@ -1,16 +1,28 @@
 defmodule BeamCraft.GameServer do
   use GenServer
 
+  @server_name "Beam Craft Server"
+  @server_motd "This is a test server!"
+
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
   def init(:ok) do
-    {:ok, %{}}
+    {:ok, %{clients: []}}
   end
 
   # handle login
-  def handle_call({username, password}, state}) do
-    {:ok, { "server name", "server motd", :usertype_regular }, state}
+  def handle_call({_username, _password}, from, state) do
+    {:reply,
+      { @server_name, @server_motd, :usertype_regular },
+      %{state|clients: [from|state.clients]}
+  }
+  end
+
+  ## client stuff
+  def login(username, password) do
+    server_pid = :erlang.whereis(__MODULE__)
+    GenServer.call( server_pid, {username, password})
   end
 end
